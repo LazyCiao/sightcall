39cf65390c5f2638fccf5f96406b7ab4aecc3ff1
Logan Scott <123471880+LazyCiao@users.noreply.github.com>
2023-12-12 13:26:36 +0100

    lat + lon fetch cancellation

--- sightcall_django/backend/views.py
+++ sightcall_django/backend/views.py
@@ -9,15 +9,10 @@ class WeatherDataListCreateView(generics.ListCreateAPIView):
     def get_queryset(self):
         # Get parameters from URL
         city = self.kwargs.get('city')
-        lat = self.request.query_params.get('lat')
-        lon = self.request.query_params.get('lon')
 
         if city:
             # If city is provided, get weather data by city name
             return self.get_weather_data_by_city(city)
-        elif lat and lon:
-            # If latitude and longitude are provided, get weather data by coordinates
-            return self.get_weather_data_by_coordinates(lat, lon)
         else:
             # Return an empty list if no valid parameters are provided
             return []
@@ -26,11 +21,6 @@ class WeatherDataListCreateView(generics.ListCreateAPIView):
         # Create URL for weather data by city
         url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPENWEATHERMAP_API_KEY}'
         return self.make_api_call(url, city)
-
-    def get_weather_data_by_coordinates(self, lat, lon):
-        # Create URL for weather data by coordinates
-        url = f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={OPENWEATHERMAP_API_KEY}'
-        return self.make_api_call(url, f'{lat},{lon}')
 
     def make_api_call(self, url, identifier):
         # Make API call to OpenWeatherMap
